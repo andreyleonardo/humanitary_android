c25e06bb06893163e7e63b09c8714396195cce1b
Daniel Marchena <daniel@Daniels-MacBook-Pro.local>
2015-11-06 00:21:03 -0200

    Fix to get nearby array

--- app/src/main/java/br/edu/humanitary/ConnectionUtils.java
+++ app/src/main/java/br/edu/humanitary/ConnectionUtils.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -24,18 +25,18 @@ public class ConnectionUtils extends AsyncTask<Object, Void, StringBuffer> {
         try{
             URL url = new URL(urlString);
             HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-            connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            connection.setRequestProperty("Content-Type", "application/json");
             connection.setRequestProperty("Accept", "application/json");
             connection.setRequestMethod("POST");
-            connection.setDoInput(true);
+            connection.setDoOutput(true);
             connection.connect();
 
-            OutputStreamWriter wr= new OutputStreamWriter(connection.getOutputStream());
-            wr.write(json.toString());
+            BufferedWriter out =
+                    new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+            out.write(json.toString());
+            out.close();
 
-            InputStream inputStream = connection.getInputStream();
-
-            BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream));
+            BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String line = "";
             while ((line = rd.readLine()) != null) {
                 System.out.println(line);
